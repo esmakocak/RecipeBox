c72c50e0aa4ff3dcfb277ee06cd59f87925bab6b
Esma Koçak <esma.kocaakk20@gmail.com>
2024-07-20 23:27:58 +0300

    Initial Commit

---
+++ RecipeSaver/RecipeSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeSaverApp.swift
+//  RecipeSaver
+//
+//  Created by Esma Koçak on 20.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct RecipeSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
