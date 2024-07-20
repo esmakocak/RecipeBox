e0573b9879d9cf5efc38cb8c55634246f80f6bca
Esma Koçak <esma.kocaakk20@gmail.com>
2024-07-20 23:38:29 +0300

    Created file structure

---
+++ RecipeSaver/Views/Main/CategoriesView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoriesView.swift
+//  RecipeSaver
+//
+//  Created by Esma Koçak on 20.07.2024.
+//
+
+import SwiftUI
+
+struct CategoriesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoriesView()
+}
---
+++ RecipeSaver/Views/Main/NewRecipeView.swift
@@ -0,0 +1,18 @@
+//
+//  NewRecipeView.swift
+//  RecipeSaver
+//
+//  Created by Esma Koçak on 20.07.2024.
+//
+
+import SwiftUI
+
+struct NewRecipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewRecipeView()
+}
