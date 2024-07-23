9dc48a142bfb2afdd04ab34c198738ffbeedb08b
Esma Koçak <esma.kocaakk20@gmail.com>
2024-07-23 15:47:01 +0300

    Finished recipe and category views

---
+++ RecipeSaver/Views/Detail/CategoryView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryView.swift
+//  RecipeSaver
+//
+//  Created by Esma Koçak on 23.07.2024.
+//
+
+import SwiftUI
+
+struct CategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryView()
+}
